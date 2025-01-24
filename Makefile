e71c6442930b206b7c867e6531e003e29eaadac5
aidosgal <agalimzhan928@gmail.com>
2025-01-24 23:35:15 +0500

    feat(sdl): add init the sdl window

--- Makefile
+++ Makefile
@@ -1,4 +1,5 @@
-CFLAGS=-Wall -Wextra -std=c11 -pedantic -ggdb
+CFLAGS=-Wall -Wextra -std=c11 -pedantic -ggdb `pkg-config --cflags sdl2`
+LIBS=`pkg-config --libs sdl2`
 
 zet: src/main.c
-	$(CC) $(CFLAGS) -o ./bin/zet src/main.c
+	$(CC) $(CFLAGS) -o ./bin/zet src/main.c $(LIBS) -lm
--- src/main.c
+++ src/main.c
@@ -1,7 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+
+#include <SDL.h>
+
+void scc(int code) {
+    if (code > 0) {
+        fprintf(stderr, "SDL ERROR: %s\n", SDL_GetError());
+        exit(1);
+    }
+}
+
+void *scp(void *ptr) {
+    if (ptr == NULL) {
+        fprintf(stderr, "SDL ERROR: %s\n", SDL_GetError());
+        exit(1);
+    }
+
+    return ptr;
+}
 
 int main(void) {
-    printf("Hello World");
+    scc(SDL_Init(SDL_INIT_VIDEO));
+
+    SDL_Window *window = 
+        scp(SDL_CreateWindow("Zet", 0, 0, 800, 600, 
+                SDL_WINDOW_RESIZABLE));
+
+    SDL_Renderer *renderer = 
+        scp(SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED));
+
+    bool quit = false;
+    while(!quit) {
+        SDL_Event event = {0};
+        while(SDL_PollEvent(&event)) {
+            switch (event.type) {
+            case SDL_QUIT: {
+                quit = true;
+            } break;
+            }
+        }
+
+        scc(SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0));
+        scc(SDL_RenderClear(renderer));
+
+        SDL_RenderPresent(renderer);
+    }
+
+    SDL_Quit();
     return 0;
 }
