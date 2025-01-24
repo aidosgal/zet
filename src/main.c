9edcdb3bd024776a6d32ab90fa96bbc05bef2127
aidosgal <agalimzhan928@gmail.com>
2025-01-25 01:05:49 +0500

    feat(font): add font texture

--- src/main.c
+++ src/main.c
@@ -4,6 +4,9 @@
 
 #include <SDL.h>
 
+#define STB_IMAGE_IMPLEMENTATION
+#include "../include/stb_image.h"
+
 void scc(int code) {
     if (code > 0) {
         fprintf(stderr, "SDL ERROR: %s\n", SDL_GetError());
@@ -20,6 +23,33 @@ void *scp(void *ptr) {
     return ptr;
 }
 
+SDL_Surface *surface_from_file(const char *file_path) {
+    int w, h, n;
+    unsigned char *pixels = stbi_load(file_path, &w, &h, &n, STBI_rgb_alpha);
+    if (pixels == NULL) {
+        fprintf(stderr, "ERROR: could not load file %s: %s\n", 
+                file_path, SDL_GetError());
+        exit(1);
+    }
+
+#if SDL_BYTEORDER == SDL_BIG_ENDIAN
+    const Uint32 rmask = 0xff000000;
+    const Uint32 gmask = 0x00ff0000;
+    const Uint32 bmask = 0x0000ff00;
+    const Uint32 amask = 0x000000ff;
+#else 
+    const Uint32 rmask = 0x000000ff;
+    const Uint32 gmask = 0x0000ff00;
+    const Uint32 bmask = 0x00ff0000;
+    const Uint32 amask = 0xff000000;
+#endif
+
+    int depth = 32;
+    int pitch = 4*w;
+    
+    return scp(SDL_CreateRGBSurfaceFrom((void*)pixels, w, h, depth, pitch, rmask, gmask, bmask, amask));
+}
+
 int main(void) {
     scc(SDL_Init(SDL_INIT_VIDEO));
 
@@ -29,6 +59,10 @@ int main(void) {
 
     SDL_Renderer *renderer = 
         scp(SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED));
+    
+    SDL_Surface *font_surface = surface_from_file("../assets/charmap-oldschool_white.png");
+
+    SDL_Texture *font_texture = SDL_CreateTextureFromSurface(renderer, font_surface);
 
     bool quit = false;
     while(!quit) {
